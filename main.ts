d550451b9d10d4d484a3d452b8fd2b0f3201a076
sufyanali3014752 <160301859+sufyanali3014752@users.noreply.github.com>
2024-02-22 12:09:45 +0500

    My 2nd practice code

--- main.ts
+++ main.ts
@@ -1,7 +1,21 @@
-// Question1
-//console.log("hollow world");
-
-// Question2
-//let myName='sufyan';
-//let massage='is good boy';
-//console.log(myName+massage);
+let score = 74;
+let grade;
+if (score >= 80 && score <= 100) {
+  grade = 'A1'  
+}
+else if (score >=70 && score <= 79){
+grade = 'A';
+}
+else if (score >= 60 && score <= 69){
+    grade = 'B'
+}
+else if (score >= 50 && score <= 59){
+    grade = 'c'
+}
+else if (score >= 50 && score <=59){
+   grade = 'D'
+}
+else if (score >= 40 && score <= 49){
+   grade = 'F'
+}
+console.log("Accroding to your score, your grade was:", grade);
